e98e72797394b465d6e9e4979d34afbb4be47127
Darryl Kilzer <darrylkilzer@gmail.com>
2019-06-24 14:13:55 -0600

    init

---
+++ app/components/CharacterController.js
@@ -0,0 +1,15 @@
+import CharacterService from "./CharacterService.js";
+
+let _characterService = new CharacterService()
+
+function drawCharacters(){
+// TODO make this draw on the page
+}
+
+export default class CharacterController{
+  constructor(){
+    _characterService.addSubscriber("characters", drawCharacters)
+    _characterService.getCharacters()
+    _characterService.getCharacter(1)
+  }
+}
